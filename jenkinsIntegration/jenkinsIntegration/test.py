e425e1842c5ff5210d59fc58f59c1f8127bb5f55
nishantbeans <nishant.nigam@infobeans.com>
2017-03-09 17:52:34 +0530

    Adding more test cases

--- jenkinsIntegration/jenkinsIntegration/test.py
+++ jenkinsIntegration/jenkinsIntegration/test.py
@@ -2,4 +2,7 @@ from django.test import TestCase
 
 class BasicTestCase(TestCase):
     def test_addition(self):
-        self.assertEqual(1+2, 4)
+        self.assertEqual(1 + 2, 4)
+
+    def test_sub(self):
+        self.assertEqual(5 - 2, 4)
