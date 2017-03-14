979bbb20e1911c2db784292197e813cb8b606875
nishantbeans <nishant.nigam@infobeans.com>
2017-03-14 15:10:04 +0530

    Adding reporting with django

--- jenkinsIntegration/jenkinsIntegration/settings.py
+++ jenkinsIntegration/jenkinsIntegration/settings.py
@@ -119,3 +119,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
--- jenkinsIntegration/jenkinsIntegration/test.py
+++ jenkinsIntegration/jenkinsIntegration/test.py
@@ -1,4 +1,7 @@
 from django.test import TestCase
+import unittest
+import xmlrunner
+
 
 class BasicTestCase(TestCase):
     def test_addition(self):
@@ -6,3 +9,12 @@ class BasicTestCase(TestCase):
 
     def test_sub(self):
         self.assertEqual(5 - 2, 4)
+
+if __name__ == '__main__':
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
+
+if __name__ == '__main__':
+    with open('results.xml', 'wb') as output:
+        unittest.main(
+            testRunner=xmlrunner.XMLTestRunner(output=output),
+            failfast=False, buffer=False, catchbreak=False)
