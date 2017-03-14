353786d80310649d8cf2c4380c7f78ca9aac10d4
nishantbeans <nishant.nigam@infobeans.com>
2017-03-14 11:36:52 +0530

    Removing xml runner

--- jenkinsIntegration/jenkinsIntegration/settings.py
+++ jenkinsIntegration/jenkinsIntegration/settings.py
@@ -119,7 +119,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.run_tests'
-TEST_OUTPUT_VERBOSE = True
-TEST_OUTPUT_DESCRIPTIONS = True
-TEST_OUTPUT_DIR = 'xmlrunner'
